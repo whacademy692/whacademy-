7bcb6052e62972f436127b5cdc84cadcaeec289f
whacademy692 <whacademy692@gmail.com>
2026-08-02 14:14:42 +0500

    Update sw.js

--- games/sw.js
+++ games/sw.js
@@ -43,7 +43,7 @@
  */
 
 // ⚠️  BUMP THIS on every frontend deploy. See the note above.
-const CACHE_VERSION = 'wha-v34';
+const CACHE_VERSION = 'wha-v35';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 
 const STATIC_ASSETS = [
