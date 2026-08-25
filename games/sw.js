e1b44782ebdb7a6a7d36f6cd2719fa99bb79cf27
whacademy692 <whacademy692@gmail.com>
2026-08-25 21:47:47 +0500

    Update CACHE_VERSION to 'wha-v66'

--- games/sw.js
+++ games/sw.js
@@ -43,7 +43,7 @@
  */
 
 // ⚠️  BUMP THIS on every frontend deploy. See the note above.
-const CACHE_VERSION = 'wha-v65';
+const CACHE_VERSION = 'wha-v66';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 
 const STATIC_ASSETS = [
